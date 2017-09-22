f2506dae3503758005c9be655fed2df40ab257fb
Devin Ryan <devin.ryan7@gmail.com>
2017-09-22 10:44:29 -0500

    made grid

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+console.log("checking links");
 // wait for the DOM to finish loading
 $(document).ready(function() {
+  var = document.querySelectorAll(".col-md-4");
   // all code to manipulate the DOM
   // goes inside this function
-
 });
+
+
