aedcd906abf62b30da27ac0888864f5fe51f2395
Devin Ryan <devin.ryan7@gmail.com>
2017-09-22 14:08:14 -0500

    made first function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,28 @@
 console.log("checking links");
 // wait for the DOM to finish loading
-$(document).ready(function() {
-  var = document.querySelectorAll(".col-md-4");
+//$(document).ready(function() {
+
   // all code to manipulate the DOM
   // goes inside this function
+//});
+
+/*function boxClick() {
+  var boxes = document.querySelectorAll(".col-md-4");
+  boxes.addEventListener('click', 'boxClick');
+    console.log("clicked a box");
+    };
+*/
+
+var boxes = [...document.querySelectorAll(".col-md-4")];
+//for each box
+// forEach, used for arrays
+boxes.forEach(function(box){
+  //set up an eventlistener
+  box.addEventListener("click", function(event){
+    //do something each time this is clicked
+    console.log("clicked a box");
+  });
 });
+
 
 
